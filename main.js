4f50986d56d9bde6b1e9af29ca68de2087f35b7f
Bayoura <Kristin.Seidelmann@gmx.de>
2022-01-19 18:54:10 +0100

    add compute and equals functions. also all clear and delete buttons. refine display functions

--- main.js
+++ main.js
@@ -12,31 +12,81 @@ class Calculator {
     }
 
     delete() {
-        
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
 
     appendNumber(number) {
-        if (number === '.' && this.currentOperand.includes('.')) return
+        if (number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperator(operator) {
-        if (this.currentOperand === '') return
+        if (this.currentOperand === '' || this.currentOperand === '.') return;
         if (this.previousOperand !== '') {
             this.compute();
         }
         this.operator = operator;
-        this.previousOperand = this.currentOperand /*+ operator;*/
+        this.previousOperand = this.currentOperand + ' ' + operator;
         this.currentOperand = '';
     }
 
     compute() {
+        let result;
+        const prevOp = parseFloat(this.previousOperand);
+        const currOp = parseFloat(this.currentOperand);
+        if (isNaN(prevOp) || isNaN(currOp)) return;
+        switch(this.operator) {
+            case '+':
+                result = prevOp + currOp;
+                break;
+            case '-':
+                result = prevOp - currOp;
+                break;
+            case '*':
+                result = prevOp * currOp;
+                break;
+            case '÷':
+                result = prevOp / currOp;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = result;
+        this.operator = undefined;
+        this.previousOperand = '';
+    }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        // if there is a decimal place (dp) in our stringNumber, it is split into the digits before and after the dp
+        // even if there is no dp, integerDigits will not be undefined. it will be the whole number
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        // if there is no dp, decimalDigits will be undefined (equal null)
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            // toLocaleString formats a string according to a given language (in this case english)
+            integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0});   
+        } 
+        // check if there are any decimal digits
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
     }
 
     updateDisplay()  {
-        this.bottomDisplay_div.innerText = this.currentOperand;
-        this.topDisplay_div.innerText = this.previousOperand;
+        this.bottomDisplay_div.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operator == null) {
+            this.topDisplay_div.innerText = '';
+        } else {
+            this.topDisplay_div.innerText = this.getDisplayNumber(this.previousOperand);
+        }
     }
 }
 
@@ -62,4 +112,19 @@ operator_buttons.forEach(button => {
         calculator.chooseOperator(button.innerText);
         calculator.updateDisplay();
     })
+})
+
+equals_button.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClear_button.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+delete_button.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
 })
